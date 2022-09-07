ef0b8e49f18561495deaa3ab67de71f6977907a8
czp356 <czp356@gmail.com>
2022-09-07 16:05:11 +0800

    Initial Commit

---
+++ WebCatTest/WebCatTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebCatTestApp.swift
+//  WebCatTest
+//
+//  Created by 常志平 on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WebCatTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
